dbb7530ca024c0a7768705b916b4fe42da26773a
MarcoMadeddu <marcomade99@gmail.com>
2020-04-10 18:22:17 +0200

    add keycode

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
 var first = $ (".first");
 var last = $(".last");
 var left = $ (".left-row");
@@ -6,49 +7,67 @@ var right = $ (".right-row");
 var firstPointer = $(".first-p");
 var lastPointer = $(".last-p");
 
-//click su freccia destra per scorrere le immagini e il relativo Pointer
+//**MAIN**
 
- right.click (function(){
-   var currentImg = $ (".active");
-   var pointer= $(".select");
+left.click(function(){
+  goLeft();
+});
 
-//rimuovere lo stato active all'immagine corrente e aggiungerla alla prossima
-   currentImg.removeClass('active');
-   currentImg.next().addClass("active");
+right.click(function(){
+  goRight();
+});
 
-//rimuovere lo stato active al pointer e aggiungerlo al sucessivo
-   pointer.removeClass("select");
-   pointer.next().addClass("select");
+$(document).keydown(function(x){
+  if(x.keyCode == 39){
+    goRight();
+  }else if (x.keyCode == 37) {
+    goLeft();
+  }
+});
 
-//controllo per verificare che si tratti dell'ultimo elemento, se così fosse si settano i valori di active e select ai primi elementi
-   if(currentImg.hasClass("last")){
-     first.addClass("active");
-     firstPointer.addClass("select");
-   }
+//**FUNCTIONS**
 
- });
+function goRight(){
+  //click su freccia destra per scorrere le immagini e il relativo Pointer
+     var currentImg = $ (".active");
+     var pointer= $(".select");
 
+  //rimuovere lo stato active all'immagine corrente e aggiungerla alla prossima
+     currentImg.removeClass('active');
+     currentImg.next().addClass("active");
 
-//click su freccia sinistra per scorrere le immagini e il relativo Pointer
- left.click (function(){
+  //rimuovere lo stato active al pointer e aggiungerlo al sucessivo
+     pointer.removeClass("select");
+     pointer.next().addClass("select");
 
-   var currentImg = $ (".active");
-   var pointer = $ (".select");
+  //controllo per verificare che si tratti dell'ultimo elemento, se così fosse si settano i valori di active e select ai primi elementi
+     if(currentImg.hasClass("last")){
+       first.addClass("active");
+       firstPointer.addClass("select");
+     }
 
-//rimuovere lo stato active all'immagine corrente e aggiungerla alla precedente
-   currentImg.removeClass("active");
-   currentImg.prev().addClass("active");
+   };
 
-//rimuovere lo stato active al pointer e aggiungerlo al precedente
-   pointer.removeClass("select");
-   pointer.prev().addClass("select");
+ function goLeft(){
+   //click su freccia sinistra per scorrere le immagini e il relativo Pointer
 
-//controllo per verificare che si tratti del primo elemento, se così fosse si settano i valori di active e select agli ultimi elementi
-   if(currentImg.hasClass("first")){
-     last.addClass("active");
-     lastPointer.addClass("select");
-   }
+      var currentImg = $ (".active");
+      var pointer = $ (".select");
 
-  });
+   //rimuovere lo stato active all'immagine corrente e aggiungerla alla precedente
+      currentImg.removeClass("active");
+      currentImg.prev().addClass("active");
+
+   //rimuovere lo stato active al pointer e aggiungerlo al precedente
+      pointer.removeClass("select");
+      pointer.prev().addClass("select");
+
+   //controllo per verificare che si tratti del primo elemento, se così fosse si settano i valori di active e select agli ultimi elementi
+      if(currentImg.hasClass("first")){
+        last.addClass("active");
+        lastPointer.addClass("select");
+      }
+
+     };
 
 });
